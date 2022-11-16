a73ccff78f411714e3a8a5398376b1b898a56faf
Daniel <danieldwoodward@gmail.com>
2022-11-16 07:15:13 -0700

    buttons are stored in local storage and are being preduced on pageload and also populated day 1 of 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,58 @@
 var apiKey = "05da83faa28532e9953c078bb342b0b7";
 var cityInput = "";
-
+historyArray = [];
+var hisIndex = 0;
 
 $(document).ready(function () {
+  var locHis = localStorage.getItem("historyArray");
+  var locHisArray = locHis.split(",");
+  console.log(locHisArray);
 
+  historyArray = locHisArray;
 
+  locHisArray.forEach(function (Element) {
+    var selectUl = document.getElementById("buttonMemory");
+    var createLi = document.createElement("li");
 
-        $("#submitButton").on("click", function (event) {
-                event.preventDefault();
-            
-                var inputValue = $("#city-input").val();
+    selectUl.setAttribute("class", "row");
 
-                inputValue.empty();
+    createLi.setAttribute("class", "btn col-10");
+    createLi.setAttribute("id", Element);
+    createLi.textContent = Element;
 
-                console.log(inputValue);
+    selectUl.appendChild(createLi);
+  });
 
-                baseFunction(inputValue);
-        });
+  $("#submitButton").on("click", function (event) {
+    event.preventDefault();
 
-        // $("ul li").on ("click", function (event){
-        //         event.preventDefault();
+    var inputValue = $("#city-input").val();
 
-                
-        // })
+    baseFunction(inputValue);
 
-  
-  
-function baseFunction(inputValue){
+    var selectUl = document.getElementById("buttonMemory");
+    var createLi = document.createElement("li");
+
+    selectUl.setAttribute("class", "row");
+
+    createLi.setAttribute("class", "btn col-10");
+    createLi.setAttribute("id", inputValue);
+    createLi.textContent = inputValue;
+
+    selectUl.appendChild(createLi);
+
+    historyArray.push(inputValue);
+    console.log(historyArray);
+  });
+
+  $("#buttonMemory").on("click", function (event) {
+    event.preventDefault();
+    console.log("buttonpress");
+    baseFunction(event.target.id);
+  });
+
+  function baseFunction(inputValue) {
+    $("#city-input").val("");
 
     var cityInput = inputValue.split(" ").join("+");
 
@@ -57,7 +83,7 @@ function baseFunction(inputValue){
         todayTemp.textContent = tTemp;
 
         var tWind = data.wind.speed;
-        tWind = Math.round(tWind*2.23694) + " mph";
+        tWind = Math.round(tWind * 2.23694) + " mph";
 
         todayWind.textContent = tWind;
 
@@ -65,37 +91,51 @@ function baseFunction(inputValue){
 
         todayHumidity.textContent = tHumidity + "%";
 
-        console.log(inputValue);
+        console.log(historyArray);
 
+        localStorage.setItem("historyArray", historyArray);
 
-        var selectUl = document.getElementById("buttonMemory");
-        var createLi = document.createElement("li");
+        var queryUrl5Day =
+          "https://api.openweathermap.org/data/2.5/forecast?q=" +
+          cityInput +
+          "&appid=" +
+          apiKey;
 
-        selectUl.setAttribute("class","row")
+        fetch(queryUrl5Day)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
 
-        createLi.setAttribute("class","btn col-10")
-        createLi.textContent = inputValue;
-        
-        selectUl.appendChild(createLi);
+            var dateD1Id = document.getElementById("date1");
+            var emojiD1Id = document.getElementById("emoji1");
+            var tempD1Id = document.getElementById("tempD1");
+            var windD1Id = document.getElementById("windD1");
+            var humidityD1Id = document.getElementById("humidity1");
 
-        var createdButton = {
-                name: inputValue,
-                windSpeed: tWind,
-                temputure: tTemp,
-                humidity: tHumidity,
-        }
+            var date1 = data.list[0].dt_txt;
+            date1Array = date1.split(" ");
+            dateD1Id.textContent = date1Array[0];
 
+            var emojiD1 = data.list[0].weather[0].icon;
+            emojiD1Id.setAttribute(
+              "src",
+              "http://openweathermap.org/img/wn/" + emojiD1 + "@2x.png"
+            );
 
+            var tempD1 = data.list[0].main.temp;
+            tempD1 = (Math.round(tempD1 - 273.15) * 9) / 5 + 32;
+            tempD1 = tempD1 + " deg F";
+            tempD1Id.textContent = "Temp: " + tempD1;
 
+            var windD1 = data.list[0].wind.speed;
+            windD1 = Math.round(windD1 * 2.23694) + " mph";
+            windD1Id.textContent = "Wind: " + windD1;
 
-              localStorage.setItem("createdButton", JSON.stringify(createdButton));
-
-
-        
+            var humidityD1 = data.list[0].main.humidity;
+            humidityD1Id.textContent = "Humid: " + humidityD1 + "%";
+          });
       });
-
-  };
-
+  }
 });
-
-
